33a4ada3250794eebd3eae9c2b1561217ef9e6c7
John Chen <john.chan08@yahoo.com>
2018-07-04 10:33:47 -0500

    reformat code, add readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EasyCook
+------------------------------------
+The project was done by Fall 2017 as a group project in CS 411 Database System. 
+
+I improved the web app in Python 3 during the summer in 2018.
+
+# Usage
+------------------------------------
+```
+python ecapp.py
+```
--- nr.py
+++ nr.py
@@ -20,27 +20,16 @@ for entry in json_data:
 	i += 1
 	print("i", i)
 	title = entry['title']
-	#print(title)
 	ingredients = entry['ingredients']
 	
 	cursor.execute('''SELECT name_R From Recipe Where name_R='%s';'''%(thwart(title)))
 	check = cursor.fetchall()
 	if check!=():
 		continue
-	#print(type(ingredients))
-	#for j in ingredients:
-		#print(j)
-		#print('''INSERT INTO Ingredient(name_I, name_R) VALUES('%s', '%s');'''%(MySQLdb.escape_string(j),MySQLdb.escape_string(title)))
-		#cursor.execute('''INSERT INTO Ingredient(name_I, name_R) VALUES('%s', '%s');'''%(MySQLdb.escape_string(j), MySQLdb.escape_string(title)))
-		#connection.commit()
-	#print(ingredients)
 	st ="['"+ "', '".join(ingredients)+"']"
 	print(st)
-	#print('''INSERT INTO aaa (name_R,ingredients,categories,rating) VALUES('%s', '%s', '%s',0);'''%(title, st, st))
 	cursor.execute('''INSERT INTO Recipe (name_R,ingredients,categories,rating) VALUES('%s', '%s', '%s',0);'''%(thwart(title), thwart(st), thwart(st)))
 	connection.commit()
-	#if i==1000:
-	#	break
 
 cursor.close()
 connection.close()
